6e148437a19bac73ec2c2df3b7ba0a846dc19d54
DamienLKT <damienlim82@gmail.com>
2022-01-20 12:14:08 +0800

    navbar

---
+++ client/blog-ui/src/pages/home/Hompage.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/header/Header";
+
+const Hompage = () => {
+  return (
+    <div className="home">
+      <Header />
+    </div>
+  );
+};
+
+export default Hompage;

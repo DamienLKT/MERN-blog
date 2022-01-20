7a19ffbdff2f7d9afcec8f5cff6aa9a1e3715bb8
erlenec <73863896+erlenec@users.noreply.github.com>
2022-01-20 13:44:22 +0800

    createpostpage draft1

---
+++ client/blog-ui/src/pages/createPost/Createpostpage.jsx
@@ -0,0 +1,11 @@
+import Createform from "../../components/createform/Createform";
+
+const CreateformPage = () => {
+  return (
+    <div className="createform">
+      <Createform />
+    </div>
+  );
+};
+
+export default CreateformPage;

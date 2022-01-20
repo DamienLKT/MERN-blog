e27012f470b32e038ee527d73e4b64cec4b0c41b
DamienLKT <damienlim82@gmail.com>
2022-01-20 16:04:56 +0800

    get all posts from server side

--- client/blog-ui/src/axios/posts.js
+++ client/blog-ui/src/axios/posts.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
-export default axios.create({
-    baseURL:"http://localhost:5000"
-});
+const BASE_URL = "http://localhost:5000/api/";
+const TOKEN = ""
+
+export const publicRequest = axios.create({
+    baseURL: BASE_URL,
+  });
+  
+  export const userRequest = axios.create({
+    baseURL: BASE_URL,
+    header: { token: `Bearer ${TOKEN}` },
+  });
--- client/blog-ui/src/components/post/Post.jsx
+++ client/blog-ui/src/components/post/Post.jsx
@@ -1,9 +1,9 @@
 import "./post.css";
 
-export default function Post({ img }) {
+export default function Post({ post }) {
   return (
     <div className="post">
-      <img className="postImg" src={img} alt="" />
+      <img className="postImg" src={post.post_image} alt="" />
       <div className="postInfo">
         {/* <div className="postCats"> */}
           {/* <span className="postCat">            
@@ -14,16 +14,13 @@ export default function Post({ img }) {
           </span> */}
         {/* </div> */}
         <span className="postTitle">          
-            Lorem ipsum dolor sit amet         
+            {post.post_title}        
         </span>
         <hr />
         {/* <span className="postDate">1 hour ago</span> */}
       </div>
       <p className="postDesc">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
-        officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
-        fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
-        atque, exercitationem quibusdam, reiciendis odio laboriosam?
+        {post.post_description}
       </p>
     </div>
   );

406124e48ebe7cfb018283ec0df5efe0cd4840f7
erlenec <73863896+erlenec@users.noreply.github.com>
2022-01-20 13:43:10 +0800

    createpostform draft1

---
+++ client/blog-ui/src/components/createform/Createform.jsx
@@ -0,0 +1,36 @@
+import "./createform.css";
+
+const Createform = () => {
+  return (
+    <div className="createForm">
+      <div className="createFormTitle">
+        <span className="createFormTitleLg">Create Post</span>
+      </div>
+      <img
+        className="backgroundImg"
+        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt=""
+      />
+      <button 
+      type="button" 
+      className="button">
+        <span className="createFormSm">Add Image</span>
+      </button>
+
+      <button 
+      type="button" 
+      className="button">
+        <span className="createFormSm">Post</span>
+      </button>
+
+      <button 
+      type="button" 
+      className="button">
+        <span className="createFormSm">Return Home</span>
+      </button>
+      
+    </div>
+  );
+};
+
+export default Createform;

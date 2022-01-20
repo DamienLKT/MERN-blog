6bdf1a4a2545eca101d57dc6541bda5bb4c790f5
Abrisam Durrani Mahathir <1802806@sit.singaporetech.edu.sg>
2022-01-20 14:33:10 +0800

    edited postRoutes to add in newpostroute

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express'
 const router = express.Router()
 import { getPosts } from '../controllers/postController.js'
+import { newPosts } from '../controllers/newpostController.js'
 
-router.route('/').get(getPosts)
+router.route('/').get(getPosts);
+router.route('/').post(newPosts);
 
 export default router

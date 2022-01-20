e2dc3808019d04d7f3facb5d5a151aeca1aaa2a7
Vanessa <59255274+Vanessa-24@users.noreply.github.com>
2022-01-20 14:26:20 +0800

    add links to topnavbar

--- client/blog-ui/src/components/topbar/Topbar.jsx
+++ client/blog-ui/src/components/topbar/Topbar.jsx
@@ -13,7 +13,9 @@ const Topbar = () => {
       </div>
       <div className="topCenter">
         <ul className="topList">
-          <li className="topListItem">HOME</li>
+          <li className="topListItem">
+          <Link to='/'>HOME</Link>
+            </li>
           <li className="topListItem">ABOUT</li>
           <li className="topListItem">CONTACT</li>
           <li className="topListItem">WRITE</li>
@@ -29,8 +31,12 @@ const Topbar = () => {
           />
         ) : (
           <ul className="topList">
-            <li className="topListItem">LOGIN</li>
-            <li className="topListItem">REGISTER</li>
+            <li className="topListItem">
+              <Link to='/login'>LOGIN</Link>
+              </li>
+            <li className="topListItem">
+            <Link to='/register'>REGISTER</Link>
+              </li>
           </ul>
         )}
         <i className="topSearchIcon fas fa-search"></i>

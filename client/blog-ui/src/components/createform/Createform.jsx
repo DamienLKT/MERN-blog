63e263ab3dc90389af4505b7fe481bac834aba31
Erlene Chua <erlene_chua@mymail.sutd.edu.sg>
2022-01-20 14:40:29 +0800

    createpost draft3

--- client/blog-ui/src/components/createform/Createform.jsx
+++ client/blog-ui/src/components/createform/Createform.jsx
@@ -6,28 +6,39 @@ const Createform = () => {
       <div className="createFormTitle">
         <span className="createFormTitleLg">Create Post</span>
       </div>
+      
+      <div className="inputbox">
+        <input type="text"/>
+      </div>
+
+      <div className = "buttons-createpost">
+        <button 
+        type="button" 
+        className="button">
+          <span className="createFormSm">Add Image</span>
+        </button>
+
+        <button 
+        type="button" 
+        className="button">
+          <span className="createFormSm">Post</span>
+        </button>
+
+        <button 
+        type="button" 
+        className="button">
+          <span className="createFormSm">Return Home</span>
+        </button>
+      </div>
+
+      
+      
+
       <img
         className="backgroundImg"
         src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
         alt=""
       />
-      <button 
-      type="button" 
-      className="button">
-        <span className="createFormSm">Add Image</span>
-      </button>
-
-      <button 
-      type="button" 
-      className="button">
-        <span className="createFormSm">Post</span>
-      </button>
-
-      <button 
-      type="button" 
-      className="button">
-        <span className="createFormSm">Return Home</span>
-      </button>
       
     </div>
   );
